372c19d6f1105c1bd958c2a84ed702d1a286394c
s-batish <shivanibatish@btinternet.com>
2023-10-25 09:16:55 +0000

    Edit allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-sbatish-boutiqueado-4t9ixhk0m4y.ws-eu104.gitpod.io', 'ecommerce-site-boutique-ado-9ea005f5626b.herokuapp.com']
+ALLOWED_HOSTS = ['8000-sbatish-boutiqueado-4t9ixhk0m4y.ws-eu105.gitpod.io', 'ecommerce-site-boutique-ado-9ea005f5626b.herokuapp.com']
 
 
 # Application definition
